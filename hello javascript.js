d9c8022fbcb1bc275c6daa371a4756de502da950
wch8812 <53140860+wch8812@users.noreply.github.com>
2019-08-19 13:15:43 +1200

    Create hello javascript.js

---
+++ hello javascript.js
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<h2>JavaScript test</h2>
+
+<p id="demo">Hello World!</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me！</button>
+
+</body>
+</html>
